1114ed7c96ab86be370e0d4facd68bdcea7db5d1
viroj-ch <drslamp@hotmail.com>
2019-09-29 22:07:14 +0700

    hard code active user

--- routes/api.js
+++ routes/api.js
@@ -1,58 +1,44 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
-//const user = require('../models/user')
 
 router.get('/', (req, res) => {
     res.send('From API route')
 })
 
 
+let activeUser = {email:'a', password:'a'}; //hard code for test
+
 router.post('/register', (req, res) => {
     let userData = req.body
-    console.log('POST: /register, '+ userData)
+
+    if(activeUser.email === userData.email){
+        return res.status(403).send('user have already logged in')
+    } else {
+        activeUser = userData
+        console.table(activeUser)
+    }
+
     //-- call API to save register user into database
-    
+
     //jwt
     let payload  = {subject: userData._id}
     let token = jwt.sign(payload, "secretKey")
 
     res.status(200).send({token})
-
 })
-
-function verifyToken(req, res, next){
-    console.log(req.headers)
-    if(!req.headers.authorization) {
-        return res.status(401).send('XX: Unauthorized request')
-    }
-    let token = req.headers.authorization.split(' ')[1]
-
-    console.log(token)
-    if(token==='null'){
-        return res.status(401).send('null: Unauthorized request')
-    }
-    let payload = null;
-    try {
-        payload = jwt.verify(token, 'secretKey')
-        if (!payload) {
-            return res.status(401).send('incorrect: Unauthorized request')
-        }
-    }
-    catch (err) {
-        console.log(err);
-        return res.status(401).send('incorrect: Unauthorized request')
-    }
-    req.userId = payload.subject
-    next()
-}
-
 
 router.post('/login', (req, res) =>{
     let userData = req.body
-    console.log('POST: /login, '+ userData)
+
     //-- call API to find register user whether exists in the database?
-    //res.status(200).send(userData)
+    
+    if(activeUser.email === userData.email){
+        return res.status(403).send('user have already logged in')
+    } else {
+        activeUser = userData
+        console.table(activeUser)
+    }
 
 
     //jwt
@@ -86,6 +72,30 @@ router.get('/events', (req, res) =>{
     ]
     res.json(events)
 })
+
+function verifyToken(req, res, next){
+    if(!req.headers.authorization) {
+        return res.status(401).send('XX: Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+
+    if(token==='null'){
+        return res.status(401).send('null: Unauthorized request')
+    }
+    let payload = null;
+    try {
+        payload = jwt.verify(token, 'secretKey')
+        if (!payload) {
+            return res.status(401).send('incorrect: Unauthorized request')
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(401).send('hack token: Unauthorized request')
+    }
+    req.userId = payload.subject
+    next()
+}
 
 router.get('/special', verifyToken, (req, res) =>{
     let events =[
